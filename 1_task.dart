9c8f1870d083eca9aff863eab9d57ac9c0305126
Karina Kononchuk <karinetko@icloud.com>
2025-06-21 13:03:28 +0300

    feat(DART-06): mathOperators

--- 1_task.dart
+++ 1_task.dart
@@ -1,3 +1,18 @@
 void main (){
-  
+
+ //mathOperators();
+
+}
+void mathOperators(){
+
+  double height = 30.9;
+  double width = 18.4;
+  double area = height * width;
+  double perimeter = 2 * (height + width);
+
+  print("Area: $area");
+  print("Perimeter: $perimeter");
+
+  int fullsquare = area ~/ 1;
+  print("Full squares: $fullsquare");
 }
