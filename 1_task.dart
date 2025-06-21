062b7ab2768734574d50baf346f877b7e2f2812b
Karina Kononchuk <karinetko@icloud.com>
2025-06-21 13:27:14 +0300

    feat(DART-06): comparisonOperators

--- 1_task.dart
+++ 1_task.dart
@@ -1,6 +1,8 @@
 void main (){
 
  //mathOperators();
+ //comparisonOperators();
+
 
 }
 void mathOperators(){
@@ -15,4 +17,16 @@ void mathOperators(){
 
   int fullsquare = area ~/ 1;
   print("Full squares: $fullsquare");
+}
+void comparisonOperators(){
+
+  int number = 66;
+
+  bool isGreaterThan50 = number > 50;
+  bool isLessThan100 = number < 100;
+  bool isDivisibleBy5 = number % 5 == 0;
+
+  print('Is number greater than 50?: $isGreaterThan50');
+  print('Is number less than 100?: $isLessThan100');
+  print('Is the number divisible by 5 without a remainder?: $isDivisibleBy5');
 }
